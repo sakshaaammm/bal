b43082f04b241078c95628cbb463729ffe4aca09
sakshaaammm <159638181+sakshaaammm@users.noreply.github.com>
2024-10-27 21:50:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,135 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <div className="name">ZEPTIE</div>
+      <div className="pp">Terms and Conditions</div>
+      <h1 className="heading">1. GENRAL CONDITION OF USE</h1>
+      
+      <div>
+        <h3> 1.1 Scope and Definations </h3>
+        <p>
+        These General Conditions of Use apply to all services provided by ZEPTIE (defined herein below). ZEPTIE owns and operates the Site (defined herein below) in India.
+
+Defined Terms
+
+“ZIPTIE” means Comuto SA, a company with capital of €127,312,41, registered in France under company number RCS Paris 491904546, and hereafter referred to as Comuto. The registered office of Comuto is at 84 avenue de la République, 75011 Paris, France.
+
+“Car Sharing” means the sharing of a Vehicle for a Trip by a Car Owner carrying a Co-Traveller for that Trip in exchange for a Cost Contribution;
+
+“Conditions” mean these General Conditions of Use, including the Good Conduct Charter and Privacy Policy of ZIPPE as notified on the Site.
+
+“Cost Contribution” means the amount agreed between the Car Owner and the Co-Traveler in relation to the Trip which is payable by the Co-Traveler as their contribution towards the costs of the Trip.
+
+“Co-Traveller” or “Passenger” means a Member who has accepted an offer to be transported by a Car Owner and includes all other persons who accompany such Member in the Vehicle for the Trip.
+
+“Car Owner” or “Driver” means a Member who through the Site offers to share a car journey with a Co-Traveller in exchange for the Cost Contribution.
+
+“Member” refers to a registered user of the Site.
+
+“Service” refers to any service provided by BlaBlaCar through the Site to any Member.
+
+“Site” means www.blablacar.co.in, www.blablacar.co.uk, www.blablacar.es, www.blablacar.it, www.blablacar.fr, www.ZIPPIE.nl, www.ZIPPIE.pt,www.blablacar.pl and any other website maintained or operated by BlaBlaCar which offers similar services including any microsites or sub-sites offered through any such website (e.g. “Agenda” and “Campus”).
+
+“Trip” means a given journey in relation to which a Car Owner and a Co-Traveler have agreed upon a transaction through the Site.
+
+“User Account” means an account with the Site opened by a Member and used in order to access the Service provided by ZIPPIE through the Site.
+
+“Vehicle” means the vehicle offered by a Car Owner for Car Sharing
+        </p>
+      </div>
+      <div>
+        <h3> 1.2 Acceptance of conitions </h3>
+        <p>The Conditions apply to any and all use of the Site by a Member. By using the Site, the Members signify their acceptance to these Conditions in full and agree to be bound by them .
+
+No access to the Services will be permitted unless the Conditions are accepted in full. No Member is entitled to accept part only of the Conditions. If a Member does not agree to the Conditions, such Member may not use the Services.
+
+All Members agree to comply with the Conditions and accept that their personal data may be processed in accordance with the Privacy Policy.
+
+In the event that any Member fails to comply with any of the Conditions, BlaBlaCar reserves the right, but not the obligation at its own discretion, to withdraw the User Account in question and suspend or withdraw all Services to that Member without notice.
+
+These Conditions are intended to create binding rights and obligations between Members and BlaBlaCar in accordance with the Indian Contract Act, 1872.
+
+
+
+        </p>
+        <div>
+        <h3> 1.3 Variation of Conditions,Site and Services</h3>
+          <p>ZEPTIE reserves the right to modify the Conditions at any time. In addition, ZEPTIE may vary or amend the Services provided through the Site, the Site functionality and/ or the “look and feel” of the Site at any time without notice and without liability to Members.
+
+Any modification to the Site, Services or Conditions will take effect as soon as such changes are published on the Site, subject to communication of any material change to the Conditions to the Members in an e-mail.
+
+Members will be deemed to have accepted any varied Conditions in the event that they use any Services offered through the Site following publication of the varied Conditions. Changes will not apply to any bookings which have been made prior to publication of the varied Conditions.</p>
+        </div>
+
+      
+
+      <h1> 2.USE OF SERVICE </h1>
+      <h3> 2.1 User Account and Accuracy of Information </h3>
+      <p>In order to use the Services each Member must create a User Account and agrees to provide any personal information requested by BlaBlaCar. In particular, Members will be required to provide their first name, last name, age, title, valid telephone number and email address. Use of the Site is limited to those over the age of 18 years at the time of registration.
+
+Members agree and accept that all of the information they provide to BlaBlaCar when setting up their User Account and at any other time shall be true, correct, complete and accurate in all respects. Members also agree that any information supplied to BlaBlaCar or posted on the Site in connection with any Trip, Vehicle or Car Sharing will be true, accurate and complete.
+
+Members agree and understand that BlaBlaCar does not undertake any verification to confirm the accuracy of any information provided by the Members on the Site or to a Car Owner or Co-Traveler, as the case maybe. BlaBlaCar will not be liable to any Member in the event that any information provided by another Member is false, incomplete, inaccurate, misleading or fraudulent.
+
+Unless expressly agreed by BlaBlaCar, Members are limited to one User Account per Member. No User Account may be created on behalf of or in order to impersonate another person.</p>
+      
+<h3>2.2 No Commercial Activity and Status of BlaBlaCar</h3>
+<p>The Site and the Services are strictly limited to providing a Service for Car Owners and Co-Travelers to car share in a private capacity. The Services may not be used to offer or accept car sharing for hire or reward or for profit or in any commercial or professional context. The Services may be used only to offer or accept car sharing in exchange for sharing the cost of the Trip between the Car Owner and the Co-Traveler.
+
+Car Owners agree not to obtain any hire or reward or make profit in any form, from any Trip. The Service and the Cost Contribution may only be used to discharge the Car Owner’s costs and may not be used to generate any hiring charges or reward or profit in any form for the Car Owner. The Car Owner is not entitled to make profit by virtue of the amount of the Cost Contribution, the types of Trips offered by a Car Owner, the frequency of such Trips or the number of Co-Travelers transported. This applies to all activities, arrangements and Services booked using the Site and any additional services or activities which may be agreed between Car Owner and Co-Traveler through the Site.
+
+The Car Owner must not provide any additional services to the Co-Traveler in exchange for hiring charges or any reward or for profit or otherwise (and the Co-Traveler may not accept or ask for any such services) including (without limitation) package delivery, waiting time, additional drop offs and pick-ups and collecting additional passengers (other than the Co-Traveler).
+
+All Trips, collection points and destinations must be pre-agreed through the Site between the Car Owner and Co-Traveler. Car Owners may not collect any Co-Travelers from any location which has not been pre-agreed with the Co-Traveler through the Site.
+
+Members are reminded that using the Services and offering Trips for hire or reward or in a commercial or professional capacity may invalidate a Car Owner’s insurance and invite adverse legal actions by the road transport authorities. BlaBlaCar shall not be in for any loss or damage incurred by a Member as a result of any or breach by a Member of these Conditions including where any Car Owner (in breach of these terms) offers Services through the Site in a professional or commercial capacity (thereby potentially invalidating their insurance) and breach of any agreement between the Car Owner and the Co-Traveler. Any offering of Trips in violation of the Conditions shall be at the sole risk such Member and BlaBlaCar shall have no liability towards Members for such violations.
+
+Status of BlaBlaCar
+
+Neither BlaBlaCar nor the Site provides any transport services. The Site is a communications platform for Members to transact with one another. BlaBlaCar does not interfere with Trips, destinations or timings. The agreement for car sharing is between the Car Owner and the Co-Traveler. BlaBlaCar is not a party to any agreement or transaction between Members, nor is BlaBlaCar liable in respect of any matter arising which relates to a booking between Members. BlaBlaCar is not and will not act as an agent for any Member.
+
+Any breach of these Conditions will give rise to immediate suspension of such Member’s User Account and they may be restricted from accessing any further Services.
+
+</p>
+<h3>2.3 Types of Booking and Payment</h3>
+<p>BlaBlaCar offers to its Members an online booking service (hereinafter, the “Booking Service”) with a view to facilitate the booking of seats by Passengers.
+
+BlaBlaCar reserves the right not to offer the Booking Service to a Trip o due to: (i) changes to applicable law, changes in the practice of regulatory authorities or changes in case law, (ii) changes to market practices or technology changes, (iii) changes of business considerations underlying the Booking Services, and (iv) other important and valid reasons
+
+BlaBlaCar reserves the right to limit the number and/or class of Members eligible to the Booking Service for any Trip.
+
+2. How to book a Car-Sharing for a Trip: The Car Owner provides details of his or her Trip on the Site, specifying date and time for departure and destination points, the amount of the Cost Contribution per seat and all other relevant travel conditions.
+
+The Passenger books one or more seats in the car for that Trip from the Site exclusively by clicking on the button « Book » (hereinafter, the “Booking”).
+
+BlaBlaCar will then send an email confirmation to each of the Driver and the Passenger confirming the Booking (hereinafter, the “Booking Confirmation”). Once a Booking Confirmation has been sent, the Booking is complete and a separate binding agreement for Car-Sharing relating to theTrip shall be formed between the Car-Owner and Passenger.
+
+Members accept that given the nature of the service Car Owners and Co-Travelers will have no recourse to BlaBlaCar for any aspect of the transaction including in relation to cancellation, last minute changes, failure by the Car Owner or the Co-Traveler to turn up or non-payment of the Cost Contribution. In particular it is the Car Owner’s responsibility to collect payment from the Co-Traveler at the time of the Trip.
+
+BlaBlaCar will not contact either party and will take no other steps whatsoever than those described above to manage the booking. The operation of the Trip is solely managed by the respective Car Owner and Co-Traveler(s).
+
+Please note that BlaBlaCar reserves the right to change any aspect of the Site or the Service which may include adding new services (which may require payment) or withdrawing any existing Services. BlaBlaCar does not guarantee that the Site will be functional at all times and Services may be suspended during such period when the Site is not in operation. BlaBlaCar will not be liable to any of the Members in case where the Site is non-operational.
+
+</p>
+
+
+      </div>
+      
+      </div>
+      </>
+      
+      
+    
+  )
+  
+}
+
+export default App
